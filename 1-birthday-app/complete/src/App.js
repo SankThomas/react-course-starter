03dd1d56ece1b048c2730786f8cc7e6ee6e1a2b5
Thomas Sankara <thomassankara74@gmail.com>
2021-05-14 21:14:11 +0300

    birthdays app complete

---
+++ 1-birthday-app/complete/src/App.js
@@ -0,0 +1,45 @@
+import { useState } from "react"
+import { birthdays } from "./birthdays"
+
+const App = () => {
+  const [people, setPeople] = useState(birthdays)
+
+  const handleDelete = (id) => {
+    setPeople(people.filter((person) => person.id !== id))
+  }
+
+  return (
+    <>
+      <section className="main">
+        <h1>A simple birthdays App</h1>
+        <p className="length">{people.length} people have birthdays today</p>
+        <div className="main-div">
+          {people.map(({ id, title, dob }) => (
+            <article
+              key={id}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                marginTop: 5,
+              }}
+            >
+              <div>
+                <h4>{title}</h4>
+                <p>{dob}</p>
+              </div>
+              <button onClick={() => handleDelete(id)} className="btn-delete">
+                Remove
+              </button>
+            </article>
+          ))}
+        </div>
+        <button onClick={() => setPeople([])} className="btn-clear">
+          Clear List
+        </button>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ 1-birthday-app/complete/src/birthdays.js
@@ -0,0 +1,34 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const birthdays = [
+  {
+    id: uuidv4(),
+    title: "Thomas Sankara",
+    dob: "10/10/2010",
+  },
+  {
+    id: uuidv4(),
+    title: "Thomas Sankara",
+    dob: "10/10/2010",
+  },
+  {
+    id: uuidv4(),
+    title: "Thomas Sankara",
+    dob: "10/10/2010",
+  },
+  {
+    id: uuidv4(),
+    title: "Thomas Sankara",
+    dob: "10/10/2010",
+  },
+  {
+    id: uuidv4(),
+    title: "Thomas Sankara",
+    dob: "10/10/2010",
+  },
+  {
+    id: uuidv4(),
+    title: "Thomas Sankara",
+    dob: "10/10/2010",
+  },
+]
