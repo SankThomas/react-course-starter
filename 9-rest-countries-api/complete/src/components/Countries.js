f6742fabdf2ee01c5d6358e3af37b68434308392
Thomas Sankara <thomassankara74@gmail.com>
2021-05-15 21:22:59 +0300

    loading state + render countries data

--- 9-rest-countries-api/complete/src/components/Countries.js
+++ 9-rest-countries-api/complete/src/components/Countries.js
@@ -5,13 +5,14 @@ const url = `https://restcountries.eu/rest/v2/all`
 
 const Countries = () => {
   const [countries, setCountries] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const fetchCountries = async () => {
       const response = await fetch(url)
       const data = await response.json()
       setCountries(data)
-      console.log(data)
+      setIsLoading(false)
     }
 
     fetchCountries()
@@ -20,55 +21,68 @@ const Countries = () => {
   return (
     <>
       <section>
-        <h1>{countries.length} countries</h1>
-        <div className="countries">
-          {countries.map((country) => {
-            const {
-              name,
-              alpha2Code,
-              capital,
-              region,
-              subregion,
-              population,
-              demonym,
-              timezones,
-              borders,
-              numericCode,
-              flag,
-            } = country
+        <h2
+          style={{
+            textAlign: "center",
+            marginTop: 20,
+            marginBottom: 20,
+            fontSize: 30,
+          }}
+        >
+          Showing {countries.length} countries
+        </h2>
+        {isLoading ? (
+          <div className="loader">
+            <div className="spinner"></div>
+          </div>
+        ) : (
+          <div className="countries">
+            {countries.map((country) => {
+              const {
+                name,
+                alpha2Code,
+                capital,
+                region,
+                subregion,
+                population,
+                demonym,
+                numericCode,
+                flag,
+              } = country
 
-            return (
-              <article key={numericCode}>
-                <img src={flag} alt={name} />
-                <div>
-                  <h4>
-                    {name}, {alpha2Code}
-                  </h4>
-                  <ul>
-                    <li>
-                      <span>Capital:</span> {capital}
-                    </li>
-                    <li>
-                      <span>Region:</span> {region}
-                    </li>
-                    <li>
-                      <span>SubRegion:</span> {subregion}
-                    </li>
-                    <li>
-                      <span>Population:</span> {population}
-                    </li>
-                    <li>
-                      <span>Demonym:</span> {demonym}
-                    </li>
-                    <li>
-                      <Link to={`/country/${name}`}>More Details</Link>
-                    </li>
-                  </ul>
-                </div>
-              </article>
-            )
-          })}
-        </div>
+              return (
+                <article key={numericCode}>
+                  <img src={flag} alt={name} />
+                  <div>
+                    <h4>
+                      {name}, {alpha2Code}
+                    </h4>
+                    <ul>
+                      <li>
+                        <span>Capital:</span> {capital}
+                      </li>
+                      <li>
+                        <span>Region:</span> {region}
+                      </li>
+                      <li>
+                        <span>SubRegion:</span> {subregion}
+                      </li>
+                      <li>
+                        <span>Population:</span> {population}
+                      </li>
+                      <li>
+                        <span>Demonym:</span> {demonym}
+                      </li>
+                      <li>
+                        <Link to={`/country/${name}`}>More Details</Link>
+                      </li>
+                    </ul>
+                  </div>
+                </article>
+              )
+            })}
+          </div>
+        )}
       </section>
     </>
   )
