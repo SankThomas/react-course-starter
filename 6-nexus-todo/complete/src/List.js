af3fa092ada138ca839dea51842ceedfb34d0e6f
Thomas Sankara <thomassankara74@gmail.com>
2021-05-15 22:35:24 +0300

    actual todo list

---
+++ 6-nexus-todo/complete/src/List.js
@@ -0,0 +1,23 @@
+const List = ({ items, deleteItem }) => {
+  return (
+    <>
+      <div>
+        {items.map(({ id, title }) => (
+          <ul className="flex items-center justify-between">
+            <li key={id} className="my-2 text-lg">
+              {title}
+            </li>
+            <button
+              onClick={() => deleteItem(id)}
+              className="bg-red-500 py-1 px-6 text-white"
+            >
+              Delete
+            </button>
+          </ul>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default List
