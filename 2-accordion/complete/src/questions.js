2c0f2857e100e46d54b49581c5f6cdc7f9e22efa
Thomas Sankara <thomassankara74@gmail.com>
2021-05-16 19:23:40 +0300

    dummy data

---
+++ 2-accordion/complete/src/questions.js
@@ -0,0 +1,26 @@
+export const questions = [
+  {
+    id: 1,
+    question: "Abilities forfeited situation extremely my to he resembled.",
+    answer:
+      "Old had conviction discretion understood put principles you. Match means keeps round one her quick. She forming two comfort invited. Yet she income effect edward. Entire desire way design few. Mrs sentiments led solicitude estimating friendship fat. Meant those event is weeks state it to or. Boy but has folly charm there its. Its fact ten spot drew. ",
+  },
+  {
+    id: 2,
+    question: "Abilities forfeited situation extremely my to he resembled.",
+    answer:
+      "Old had conviction discretion understood put principles you. Match means keeps round one her quick. She forming two comfort invited. Yet she income effect edward. Entire desire way design few. Mrs sentiments led solicitude estimating friendship fat. Meant those event is weeks state it to or. Boy but has folly charm there its. Its fact ten spot drew. ",
+  },
+  {
+    id: 3,
+    question: "Abilities forfeited situation extremely my to he resembled.",
+    answer:
+      "Old had conviction discretion understood put principles you. Match means keeps round one her quick. She forming two comfort invited. Yet she income effect edward. Entire desire way design few. Mrs sentiments led solicitude estimating friendship fat. Meant those event is weeks state it to or. Boy but has folly charm there its. Its fact ten spot drew. ",
+  },
+  {
+    id: 4,
+    question: "Abilities forfeited situation extremely my to he resembled.",
+    answer:
+      "Old had conviction discretion understood put principles you. Match means keeps round one her quick. She forming two comfort invited. Yet she income effect edward. Entire desire way design few. Mrs sentiments led solicitude estimating friendship fat. Meant those event is weeks state it to or. Boy but has folly charm there its. Its fact ten spot drew. ",
+  },
+]
