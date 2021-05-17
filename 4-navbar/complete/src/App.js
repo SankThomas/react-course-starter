cd549c896512a4275a7ddffad9d8d94ef15221fa
Thomas Sankara <thomassankara74@gmail.com>
2021-05-17 10:50:12 +0300

    navbar component

---
+++ 4-navbar/complete/src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import { navbar } from "./navbar"
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false)
+  // eslint-disable-next-line
+  const [links, setLinks] = useState(navbar)
+
+  return (
+    <>
+      <header>
+        <div>
+          <h1>Sankara</h1>
+        </div>
+
+        {isOpen && (
+          <nav>
+            <ol>
+              {links.map(({ id, title }) => (
+                <li key={id} onClick={() => setIsOpen(false)}>
+                  {title}
+                </li>
+              ))}
+            </ol>
+          </nav>
+        )}
+
+        <div>
+          <button onClick={() => setIsOpen(!isOpen)}>Menu</button>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default App
