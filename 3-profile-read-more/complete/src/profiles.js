827bce0cfbc279cab7a5af59bcb75575cb5564e9
Thomas Sankara <thomassankara74@gmail.com>
2021-05-17 12:19:01 +0300

    data array

---
+++ 3-profile-read-more/complete/src/profiles.js
@@ -0,0 +1,32 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const profiles = [
+  {
+    id: uuidv4(),
+    name: "Thomas Sankara",
+    desc: "Unwilling sportsmen he in questions september therefore described so. Attacks may set few believe moments was. Reasonably how possession shy way introduced age inquietude. Missed he engage no exeter of. Still tried means we aware order among on. Eldest father can design tastes did joy settle. Roused future he ye an marked. Arose mr rapid in so vexed words. Gay welcome led add lasting chiefly say looking.",
+    avatar:
+      "https://images.pexels.com/photos/3483800/pexels-photo-3483800.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    id: uuidv4(),
+    name: "John Doe",
+    desc: "Unwilling sportsmen he in questions september therefore described so. Attacks may set few believe moments was. Reasonably how possession shy way introduced age inquietude. Missed he engage no exeter of. Still tried means we aware order among on. Eldest father can design tastes did joy settle. Roused future he ye an marked. Arose mr rapid in so vexed words. Gay welcome led add lasting chiefly say looking.",
+    avatar:
+      "https://images.pexels.com/photos/7083673/pexels-photo-7083673.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    id: uuidv4(),
+    name: "Jane Doe",
+    desc: "Unwilling sportsmen he in questions september therefore described so. Attacks may set few believe moments was. Reasonably how possession shy way introduced age inquietude. Missed he engage no exeter of. Still tried means we aware order among on. Eldest father can design tastes did joy settle. Roused future he ye an marked. Arose mr rapid in so vexed words. Gay welcome led add lasting chiefly say looking.",
+    avatar:
+      "https://images.pexels.com/photos/5641225/pexels-photo-5641225.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    id: uuidv4(),
+    name: "Alice White",
+    desc: "Unwilling sportsmen he in questions september therefore described so. Attacks may set few believe moments was. Reasonably how possession shy way introduced age inquietude. Missed he engage no exeter of. Still tried means we aware order among on. Eldest father can design tastes did joy settle. Roused future he ye an marked. Arose mr rapid in so vexed words. Gay welcome led add lasting chiefly say looking.",
+    avatar:
+      "https://images.pexels.com/photos/7614400/pexels-photo-7614400.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+]
