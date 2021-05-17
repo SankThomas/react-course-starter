23076f4f0deef79ff57415e8a90012a6aa578538
Thomas Sankara <thomassankara74@gmail.com>
2021-05-17 13:08:08 +0300

    initial commit

---
+++ 8-random-user/complete/src/App.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react"
+
+function App() {
+  const [people, setPeople] = useState([])
+
+  return (
+    <>
+      <h1>Looking for a cool Alias?</h1>
+    </>
+  )
+}
+
+export default App
