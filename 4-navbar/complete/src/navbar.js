d63ec8261a681392ce476ed6fda8366dc5870863
Thomas Sankara <thomassankara74@gmail.com>
2021-05-17 10:50:25 +0300

    data array

---
+++ 4-navbar/complete/src/navbar.js
@@ -0,0 +1,22 @@
+export const navbar = [
+  {
+    id: 1,
+    title: "Home",
+  },
+  {
+    id: 2,
+    title: "About",
+  },
+  {
+    id: 3,
+    title: "Contacts",
+  },
+  {
+    id: 4,
+    title: "Hire me",
+  },
+  {
+    id: 5,
+    title: "How to reach me",
+  },
+]
