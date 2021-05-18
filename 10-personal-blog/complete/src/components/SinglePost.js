24f6e98d38ffb474f0201b3829b4507bae6d9785
Thomas Sankara <thomassankara74@gmail.com>
2021-05-18 09:15:03 +0300

    SinglePost page

--- 10-personal-blog/complete/src/components/SinglePost.js
+++ 10-personal-blog/complete/src/components/SinglePost.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import sanityClient from "../client"
 
 const SinglePost = () => {
@@ -35,11 +35,18 @@ const SinglePost = () => {
     <>
       <div>
         <section>
-          <h2>{singlePost.title}</h2>
+          <h2 className="font-bold text-2xl mb-2">{singlePost.title}</h2>
           <ul>
-            <li>{singlePost.authorName}</li>
+            <li className="text-sm">By {singlePost.authorName}</li>
           </ul>
         </section>
+
+        <Link
+          to="/blog"
+          className="bg-green-500 py-2 px-5 mt-5 inline-block text-white rounded transition-all duration-500 hover:bg-green-600"
+        >
+          Back
+        </Link>
       </div>
     </>
   )
