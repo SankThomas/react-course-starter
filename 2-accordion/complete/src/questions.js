797ce683889a391e68c23b66f6243890ce626b44
Thomas Sankara <thomassankara74@gmail.com>
2021-07-31 08:48:37 +0300

    update data array

--- 2-accordion/complete/src/questions.js
+++ 2-accordion/complete/src/questions.js
@@ -7,19 +7,19 @@ export const questions = [
   },
   {
     id: 2,
-    question: "Abilities forfeited situation extremely my to he resembled.",
+    question: "Old had conviction discretion understood put principles you?",
     answer:
       "Old had conviction discretion understood put principles you. Match means keeps round one her quick. She forming two comfort invited. Yet she income effect edward. Entire desire way design few. Mrs sentiments led solicitude estimating friendship fat. Meant those event is weeks state it to or. Boy but has folly charm there its. Its fact ten spot drew. ",
   },
   {
     id: 3,
-    question: "Abilities forfeited situation extremely my to he resembled.",
+    question: "Match means keeps round one her quick?",
     answer:
       "Old had conviction discretion understood put principles you. Match means keeps round one her quick. She forming two comfort invited. Yet she income effect edward. Entire desire way design few. Mrs sentiments led solicitude estimating friendship fat. Meant those event is weeks state it to or. Boy but has folly charm there its. Its fact ten spot drew. ",
   },
   {
     id: 4,
-    question: "Abilities forfeited situation extremely my to he resembled.",
+    question: "She forming two comfort invited?",
     answer:
       "Old had conviction discretion understood put principles you. Match means keeps round one her quick. She forming two comfort invited. Yet she income effect edward. Entire desire way design few. Mrs sentiments led solicitude estimating friendship fat. Meant those event is weeks state it to or. Boy but has folly charm there its. Its fact ten spot drew. ",
   },
