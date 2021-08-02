e3c5fd53e3546ed781fe4cd59bfbff8539a41b3b
Thomas Sankara <thomassankara74@gmail.com>
2021-08-02 12:31:26 +0300

    Navbar component

---
+++ 14-electron/complete/src/components/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <>
+      <ul className="flex uppercase tracking-widest text-sm font-semibold text-gray-600">
+        <li className="mx-1 border p-1 border-gray-400 rounded-md">Order</li>
+        <li className="mx-1 border p-1 border-gray-400 rounded-md">Gift</li>
+        <li className="ml-1 border p-1 border-gray-400 rounded-md">Stores</li>
+      </ul>
+    </>
+  )
+}
