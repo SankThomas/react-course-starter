2bad73854d9ab16e8fea9e02ad56fb5f4761661b
Thomas Sankara <thomassankara74@gmail.com>
2021-08-02 12:59:08 +0300

    update UI

--- 13-pwa/complete/src/List.js
+++ 13-pwa/complete/src/List.js
@@ -1,9 +1,14 @@
 export default function List({ items }) {
   return (
     <>
-      <ul>
+      <ul className="absolute top-20 right-5 md:right-28 lg:right-96 lg:top-24">
         {items.map(({ id, title }) => (
-          <li key={id}>{title}</li>
+          <li
+            key={id}
+            className=" bg-indigo-600 p-2 rounded-md text-white mt-2"
+          >
+            {title}
+          </li>
         ))}
       </ul>
     </>
