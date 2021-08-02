48e33232d4a55eb0dd3136584c9aa92a7e8b1eb9
Thomas Sankara <thomassankara74@gmail.com>
2021-08-02 12:33:44 +0300

    refactor component

--- 14-electron/complete/src/App.js
+++ 14-electron/complete/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import Header from "./components/Header"
+import Images from "./components/Images"
 
 function App() {
   const [hours, setHours] = useState(24)
@@ -35,57 +36,7 @@ function App() {
 
         <p className="text-center mt-4">Grab your Covfefe all day every day</p>
 
-        <div className="px-5 mt-5 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
-          <div>
-            <img
-              src="https://images.pexels.com/photos/6347/coffee-cup-working-happy.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="w-full rounded-lg"
-            />
-            <p>Medium</p>
-          </div>
-          <div>
-            <img
-              src="https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="w-full rounded-lg"
-            />
-            <p>Large</p>
-          </div>
-          <div>
-            <img
-              src="https://images.pexels.com/photos/982612/pexels-photo-982612.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="w-full rounded-lg"
-            />
-            <p>Extra large</p>
-          </div>
-
-          <div>
-            <img
-              src="https://images.pexels.com/photos/982612/pexels-photo-982612.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="w-full rounded-lg"
-            />
-            <p>Extra large</p>
-          </div>
-          <div>
-            <img
-              src="https://images.pexels.com/photos/6347/coffee-cup-working-happy.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="w-full rounded-lg"
-            />
-            <p>Medium</p>
-          </div>
-          <div>
-            <img
-              src="https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="w-full rounded-lg"
-            />
-            <p>Large</p>
-          </div>
-        </div>
+        <Images />
       </div>
     </>
   )
