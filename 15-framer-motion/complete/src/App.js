f6f9adc6c97eac520604acd7d5cad97c8185751f
Thomas Sankara <thomassankara74@gmail.com>
2021-08-03 13:17:14 +0300

    refactor App.js

--- 15-framer-motion/complete/src/App.js
+++ 15-framer-motion/complete/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { AiOutlineLine } from "react-icons/ai"
+import Buttons from "./components/Buttons"
+import Form from "./components/Form"
 
-function App() {
+// Design inspiration from Igor Pavlinski on Dribbble
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div className="bg-light absolute h-full w-1/3 bg-gray-500"></div>
+      <section className="px-5 lg:max-w-6xl lg:mx-auto">
+        <div>
+          <h1 className="text-4xl text-white lg:text-8xl pt-10">Contact Us</h1>
+          <h2 className="flex items-center lg:pl-32 text-3xl lg:text-6xl text-gray-400 mt-3 mb-10">
+            <AiOutlineLine /> Need a web developer?
+          </h2>
+        </div>
 
-export default App;
+        <div>
+          <h3 className="text-gray-300 mb-5">What can we do you for?</h3>
+          <Buttons />
+        </div>
+
+        <Form />
+      </section>
+    </>
+  )
+}
