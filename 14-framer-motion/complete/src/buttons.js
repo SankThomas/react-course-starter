f9b1fb5dee7232656c7ad06b0a3f3bfd6957dbf3
Thomas Sankara <thomassankara74@gmail.com>
2021-08-04 11:40:01 +0300

    project 14

---
+++ 14-framer-motion/complete/src/buttons.js
@@ -0,0 +1,26 @@
+export const buttons = [
+  {
+    id: 1,
+    title: "Web Design",
+  },
+  {
+    id: 2,
+    title: "Web Development",
+  },
+  {
+    id: 3,
+    title: "Mobile Apps",
+  },
+  {
+    id: 4,
+    title: "Desktop Apps",
+  },
+  {
+    id: 5,
+    title: "Tutorials",
+  },
+  {
+    id: 6,
+    title: "Online Marketing",
+  },
+]
