5aa0e7349064d0aacabf42f2c7e76ec5787e170e
Thomas Sankara <thomassankara74@gmail.com>
2021-08-09 18:13:17 +0300

    Sidebar component

---
+++ 15-dashboard/complete/src/components/Sidebar.js
@@ -0,0 +1,58 @@
+import { GrFacebook } from "react-icons/gr"
+import { CgInstagram, CgTwitter } from "react-icons/cg"
+import { FiHome } from "react-icons/fi"
+import { FaTasks } from "react-icons/fa"
+import { NavLink } from "react-router-dom"
+
+export default function Sidebar() {
+  return (
+    <>
+      <aside className="absolute left-0 top-20 bg-white py-5 px-10 h-screen hidden lg:block shadow">
+        <ul className="flex flex-col items-center justify-center">
+          <NavLink
+            to="/"
+            exact
+            className="text-gray-500 mb-10 text-2xl"
+            activeClassName="text-pink-500"
+            title="Homepage"
+          >
+            <li>
+              <FiHome />
+            </li>
+          </NavLink>
+          <NavLink
+            to="/tasks"
+            className="text-gray-500 text-2xl"
+            activeClassName="text-pink-500"
+            title="Your tasks"
+          >
+            <li>
+              <FaTasks />
+            </li>
+          </NavLink>
+        </ul>
+
+        <ul className="absolute bottom-32 flex items-center -ml-6">
+          <li className="mx-1">
+            <a href="https://facebook.com">
+              <GrFacebook className="text-blue-600 text-lg" title="Facebook" />
+            </a>
+          </li>
+          <li className="mx-1">
+            <a href="https://instagram.com/tsbsankara">
+              <CgInstagram
+                className="text-pink-500 text-lg"
+                title="Instagram"
+              />
+            </a>
+          </li>
+          <li className="mx-1">
+            <a href="https://twitter.com/TSBSankara">
+              <CgTwitter className="text-blue-500 text-lg" title="Twitter" />
+            </a>
+          </li>
+        </ul>
+      </aside>
+    </>
+  )
+}
