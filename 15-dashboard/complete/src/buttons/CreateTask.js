b135b6c74821b14d5516b9d22a39f334ee9fee00
Thomas Sankara <thomassankara74@gmail.com>
2021-08-09 21:53:46 +0300

    Create Task button

---
+++ 15-dashboard/complete/src/buttons/CreateTask.js
@@ -0,0 +1,11 @@
+import { CgMathPlus } from "react-icons/cg"
+
+export default function CreateTask() {
+  return (
+    <>
+      <button className="flex items-center">
+        Create New Task <CgMathPlus className="text-lg ml-1" />
+      </button>
+    </>
+  )
+}
