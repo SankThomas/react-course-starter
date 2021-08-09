0766a681a2436fcea1c6906672af3fe8be7b551b
Thomas Sankara <thomassankara74@gmail.com>
2021-08-09 16:45:51 +0300

    update App.js

--- 15-dashboard/complete/src/App.js
+++ 15-dashboard/complete/src/App.js
@@ -1,22 +1,27 @@
-import { useState } from "react"
+// import { useState } from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import LoginForm from "./login/LoginForm"
+import Header from "./components/Header"
+// import LoginForm from "./login/LoginForm"
+import Error from "./pages/Error"
 
 // Pages
 import Dashboard from "./pages/Dashboard"
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true)
+  // const [isLoggedIn, setIsLoggedIn] = useState(true)
 
   return (
     <Router>
-      {/* Header */}
+      <Header />
       <Switch>
         <Route path="/" exact>
-          <LoginForm isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+          {/* <LoginForm isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} /> */}
+          <Dashboard />
         </Route>
-        <Route path="/dashboard">{isLoggedIn && <Dashboard />}</Route>
-        <Route path="*">{/* <Error /> */}</Route>
+        {/* <Route path="/dashboard">{isLoggedIn && <Dashboard />}</Route> */}
+        <Route path="*">
+          <Error />
+        </Route>
       </Switch>
     </Router>
   )
