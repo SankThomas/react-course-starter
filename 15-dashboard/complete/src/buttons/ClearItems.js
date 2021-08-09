bfaf719b713d433aa70c1bc5d00c9be86ac1b58b
Thomas Sankara <thomassankara74@gmail.com>
2021-08-09 21:53:34 +0300

    clear items button

---
+++ 15-dashboard/complete/src/buttons/ClearItems.js
@@ -0,0 +1,9 @@
+export default function ClearItems() {
+  return (
+    <>
+      <button className="text-sm text-gray-400 border py-1 px-3 rounded mt-5 hover:bg-gray-200 transition-all duration-300">
+        Clear All Items
+      </button>
+    </>
+  )
+}
