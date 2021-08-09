0d218b6aae680adaaa5d1f49eee9a0a7a653a0b3
Thomas Sankara <thomassankara74@gmail.com>
2021-08-09 21:52:54 +0300

    add loading state

--- 15-dashboard/complete/src/components/QuotesGenerator.js
+++ 15-dashboard/complete/src/components/QuotesGenerator.js
@@ -4,11 +4,13 @@ const url = `https://api.chucknorris.io/jokes/random`
 
 export default function QuotesGenerator() {
   const [jokes, setJokes] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   const fetchJokes = async () => {
     const response = await fetch(url)
     const data = await response.json()
     setJokes(data)
+    setIsLoading(false)
   }
 
   useEffect(() => {
@@ -17,9 +19,13 @@ export default function QuotesGenerator() {
 
   return (
     <>
-      <article>
-        <h4 className="mt-2">{jokes.value}</h4>
-      </article>
+      {isLoading ? (
+        <h2>Loading quote...</h2>
+      ) : (
+        <article>
+          <h4 className="mt-2">{jokes.value}</h4>
+        </article>
+      )}
     </>
   )
 }
