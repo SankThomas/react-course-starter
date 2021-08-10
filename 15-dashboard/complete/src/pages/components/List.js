35fd67fd148eb85d908f370ed2a28067860a9054
Thomas Sankara <thomassankara74@gmail.com>
2021-08-10 11:59:43 +0300

    add editing functionality

--- 15-dashboard/complete/src/pages/components/List.js
+++ 15-dashboard/complete/src/pages/components/List.js
@@ -1,24 +1,32 @@
-import { MdDelete } from "react-icons/md"
+import { MdDelete, MdEdit } from "react-icons/md"
 import ClearItems from "../../buttons/ClearItems"
 
-export default function List({ tasks, deleteItem, clearItems }) {
+export default function List({ tasks, deleteItem, clearItems, editTask }) {
   return (
     <>
       <h2>You have {tasks.length} tasks left to complete</h2>
       {tasks.length !== 0 && (
-        <article className="bg-white rounded py-3 px-5 shadow mt-5">
+        <article className="bg-white rounded py-3 pr-5 pl-10 shadow mt-5">
           <ol className="list-decimal">
             {tasks.map(({ id, title }) => (
               <div className="flex items-center justify-between border-b">
                 <li key={id} className="my-2">
                   {title}
                 </li>
-                <button onClick={() => deleteItem(id)}>
-                  <MdDelete
-                    className="text-red-500 text-xl"
-                    title="Delete item"
-                  />
-                </button>
+                <div>
+                  <button onClick={() => editTask(id)} className="p-1">
+                    <MdEdit
+                      className="text-green-500 text-xl mr-2 hover:bg-green-200 rounded hover:shadow transition-all duration-300"
+                      title="Edit item"
+                    />
+                  </button>
+                  <button onClick={() => deleteItem(id)} className="p-1">
+                    <MdDelete
+                      className="text-red-500 text-xl hover:bg-red-200 rounded hover:shadow transition-all duration-300"
+                      title="Delete item"
+                    />
+                  </button>
+                </div>
               </div>
             ))}
           </ol>
