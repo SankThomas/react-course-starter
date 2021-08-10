2a40ce105b2fd5288e065f77c0616dce5d033cf4
Thomas Sankara <thomassankara74@gmail.com>
2021-08-10 11:57:59 +0300

    render Dropdown

--- 15-dashboard/complete/src/components/Header.js
+++ 15-dashboard/complete/src/components/Header.js
@@ -1,4 +1,9 @@
+import { useState } from "react"
+import Dropdown from "./Dropdown"
+
 export default function Header() {
+  const [showDropdown, setShowDropdown] = useState(false)
+
   return (
     <>
       <header className="flex items-center justify-between p-5 bg-white shadow">
@@ -9,14 +14,25 @@ export default function Header() {
         <div>
           <ul className="flex items-center justify-center">
             <li>
-              <img
-                src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="Someone's profile"
-                className="h-10 w-10 rounded-full border-2 border-pink-600"
-              />
+              <button onClick={() => setShowDropdown(!showDropdown)}>
+                <img
+                  src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                  alt="Someone's profile"
+                  className="h-10 w-10 rounded-full border-2 border-pink-600"
+                />
+              </button>
             </li>
           </ul>
         </div>
+
+        {showDropdown && (
+          <article className="absolute right-3 top-24 bg-white py-5 px-10 rounded shadow">
+            <Dropdown
+              showDropdown={showDropdown}
+              setShowDropdown={setShowDropdown}
+            />
+          </article>
+        )}
       </header>
     </>
   )
