0b236f1b6e49c3c2bf315793307154a6c45a5fca
Thomas Sankara <thomassankara74@gmail.com>
2021-08-10 11:58:09 +0300

    change quotes api

--- 15-dashboard/complete/src/components/QuotesGenerator.js
+++ 15-dashboard/complete/src/components/QuotesGenerator.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react"
 
-const url = `https://api.chucknorris.io/jokes/random`
+// const url = `https://api.chucknorris.io/jokes/random`
+const url2 = `https://goquotes-api.herokuapp.com/api/v1/random?count=1`
 
 export default function QuotesGenerator() {
   const [jokes, setJokes] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
   const fetchJokes = async () => {
-    const response = await fetch(url)
+    const response = await fetch(url2)
     const data = await response.json()
     setJokes(data)
     setIsLoading(false)
@@ -23,7 +24,21 @@ export default function QuotesGenerator() {
         <h2>Loading quote...</h2>
       ) : (
         <article>
-          <h4 className="mt-2">{jokes.value}</h4>
+          <h4>{jokes.quotes[0].text}</h4>
+          <p>
+            <em>By {jokes.quotes[0].author}</em>
+          </p>
+          <small>
+            Powered by{" "}
+            <a
+              href="https://goquotes.docs.apiary.io/#reference/get-random-quote(s)/apiv1randomcount/get-random-quote(s)"
+              className="bg-white px-1 rounded shadow hover:bg-pink-300"
+              target="_blank"
+              rel="noopenner noreferrer"
+            >
+              Go Quotes API
+            </a>
+          </small>
         </article>
       )}
     </>
