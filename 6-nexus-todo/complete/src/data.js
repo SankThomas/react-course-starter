ad10094ad336a4d1362ef5f9d0f41d255a4438ac
Thomas Sankara <thomassankara74@gmail.com>
2021-08-17 13:25:22 +0300

    dummy video data

---
+++ 6-nexus-todo/complete/src/data.js
@@ -0,0 +1,43 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const defaultVideo = [
+  {
+    id: uuidv4(),
+    link: "https://player.vimeo.com/external/451547491.sd.mp4?s=c5f14fbf65739d99ee2ace610de85929e63e6645&profile_id=139&oauth2_token_id=57447761",
+    title: "A random video I found on the web",
+    desc: "Of on affixed civilly moments promise explain fertile in. Assurance advantage belonging happiness departure so of. Now improving and one sincerity intention allowance commanded not. Oh an am frankness be necessary earnestly advantage estimable extensive. Five he wife gone ye. Mrs suffering sportsmen earnestly any. In am do giving to afford parish settle easily garret. Extremely we promotion remainder eagerness enjoyment an. Ham her demands removal brought minuter raising invited gay. Contented consisted continual curiosity contained get sex. Forth child dried in in aware do. You had met they song how feel lain evil near. Small she avoid six yet table china. And bed make say been then dine mrs. To household rapturous fulfilled attempted on so.",
+  },
+]
+
+export const data = [
+  {
+    id: uuidv4(),
+    link: "https://player.vimeo.com/external/485398802.sd.mp4?s=4acc40823b0a547aea1806b3fe0e2597940dcb06&profile_id=139&oauth2_token_id=57447761",
+    title: "Random video 1",
+    desc: "Lorem ipsum dolor sit amet...",
+  },
+  {
+    id: uuidv4(),
+    link: "https://player.vimeo.com/external/485398802.sd.mp4?s=4acc40823b0a547aea1806b3fe0e2597940dcb06&profile_id=139&oauth2_token_id=57447761",
+    title: "Random video 2",
+    desc: "Lorem ipsum dolor sit amet...",
+  },
+  {
+    id: uuidv4(),
+    link: "https://player.vimeo.com/external/485398802.sd.mp4?s=4acc40823b0a547aea1806b3fe0e2597940dcb06&profile_id=139&oauth2_token_id=57447761",
+    title: "Random video 3",
+    desc: "Lorem ipsum dolor sit amet...",
+  },
+  {
+    id: uuidv4(),
+    link: "https://player.vimeo.com/external/485398802.sd.mp4?s=4acc40823b0a547aea1806b3fe0e2597940dcb06&profile_id=139&oauth2_token_id=57447761",
+    title: "Random video 4",
+    desc: "Lorem ipsum dolor sit amet...",
+  },
+  {
+    id: uuidv4(),
+    link: "https://player.vimeo.com/external/485398802.sd.mp4?s=4acc40823b0a547aea1806b3fe0e2597940dcb06&profile_id=139&oauth2_token_id=57447761",
+    title: "Random video 5",
+    desc: "Lorem ipsum dolor sit amet...",
+  },
+]
